08d70becaef9698470c9c7585dfc35631a39d8de
Cole <Cole@example.com>
2023-11-05 16:43:42 -0700

    Changed Golf portion

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ for line in file_obj_csv:
 print("Loading golf.exe . . . \n ... \n ... \n ...")
 
 print("Welcome to the golf minigame!")
+player_1 = 1
+player_2 = 2
 player = input("How many players? (1-2)>>> ")
 if int(player) > 2:
     print("Please select a number lower than or equal to 2")
@@ -18,8 +20,14 @@ holes = input("How many holes? (Max 18)>>> ")
 if int(holes) > 18:
     print("Please select a number lower than or equal to 18")
     sys.exit()
-golf_obj = open("golf game.txt", "w")
-golf_obj.write(f"Welcome to the golf minigame!\n Players: {player} \n Number of holes: {holes} \n Now loading your game . . . \n Error: Memory access violation. Please contact administration")
+golf_obj = open("Golf game.txt", "w")
+golf_obj.write(f"Welcome to the golf minigame!\n Players: {player} \n Number of holes: {holes} \n Now loading your game . . .")
+golf_score = open("Golf Score.txt", 'w')
+golf_score.write(f"""
+=========
+Score: 38    
+=========\n Thank you for playing!""")
+sys.exit
 
 
 
