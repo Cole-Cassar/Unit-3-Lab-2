63beb6b6218e72df8c7207e52a5feb6f9cf4e032
Cole <Cole@example.com>
2023-11-05 16:28:00 -0700

    Added golf mini game

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 file_obj_csv = open("data.csv")
 # csv_data = file_obj_csv.read()
 file_obj_csv.readline()
@@ -6,3 +7,20 @@ for line in file_obj_csv:
     if len(columns) >= 10:
         temp = float(columns[9].replace('"', ''))
         print(temp)
+print("Loading golf.exe . . . \n ... \n ... \n ...")
+
+print("Welcome to the golf minigame!")
+player = input("How many players? (1-2)>>> ")
+if int(player) > 2:
+    print("Please select a number lower than or equal to 2")
+    sys.exit()
+holes = input("How many holes? (Max 18)>>> ")
+if int(holes) > 18:
+    print("Please select a number lower than or equal to 18")
+    sys.exit()
+golf_obj = open("golf game.txt", "w")
+golf_obj.write(f"Welcome to the golf minigame!\n Players: {player} \n Number of holes: {holes} \n Now loading your game . . . \n Error: Memory access violation. Please contact administration")
+
+
+
+
